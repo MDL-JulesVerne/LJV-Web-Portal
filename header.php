43a30ff601deffdef683b0ad51588450a074f97b
Matiboux <matiboux@gmail.com>
2017-03-10 18:56:21 +0100

    Added missing files
    I had forgot few files (lol)
    Renamed the v2.php file to index.php

---
+++ header.php
@@ -0,0 +1,40 @@
+<header class="navbar navbar-static-top">
+	<div class="container" style="max-width: 750px">
+		<div class="navbar-header">
+			<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="http://ljv.fr/" class="navbar-brand">Lycée Jules Verne</a>
+		</div>
+		
+		<nav class="collapse navbar-collapse bs-navbar-collapse">
+			<ul class="nav navbar-nav navbar-left">
+				<li>
+					<a href="http://ljv.fr/"><i class="fa fa-home fa-fw"></i> Home</a>
+				</li>
+				<li>
+					<a href="http://ljv.fr/about.php"><i class="fa fa-question-circle fa-fw"></i> About</a>
+				</li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+				<?php /*<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+						Logged as Matiboux							<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li>
+							<a href="http://logs.matiboux.com/account/"><i class="fa fa-user fa-fw"></i> Your account</a>
+						</li>
+						<li>
+							<a href="http://logs.matiboux.com/login/logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+						</li>
+					</ul>
+				</li>*/ ?>
+			</ul>
+		</nav>
+	</div>
+</header>
