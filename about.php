2e3068d531b34f2889792a003af9a4ffb862d042
Matiboux <matiboux@gmail.com>
2017-03-14 00:58:46 +0100

    "An open project" sounds good I think

--- about.php
+++ about.php
@@ -62,7 +62,7 @@ $cookieName = 'disableRedirect';
 				<li>Email : <a href="adrien.granjon@free.fr">adrien.granjon@free.fr</a></li>
 			</ul> <hr />
 		
-		<h2>Le projet</h2>
+		<h2>Un projet libre</h2>
 		<p><b>Le projet est désormais un projet ouvert et open-source!</b></p>
 		<p>Le code source du site internet est entièrement disponible sur <a href="https://github.com/matiboux/LJV-Web-Portal">sa page Github</a> sous la <a href="https://fr.wikipedia.org/wiki/Licence_MIT">license MIT</a>. Depuis cet endroit, vous pourrez suivre l'avancée du développement du site et peut être y contribuer, si vous avez des compétences en développement web.
 	</div>
