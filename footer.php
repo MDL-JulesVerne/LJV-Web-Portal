0d91ca86bd246e9bb1ef34d70068c61864a36f8d
Matiboux <matiboux@gmail.com>
2017-03-16 17:14:56 +0100

    Edited footer infos

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 <div id="footer">
 	<ul>
-		<li>Copyright Matiboux 2017</li>
-		<li>En collaboration avec le staff du <a href="https://nautilus.ljv.fr/">Nautilus</a></li>
+		<li>Site internet maintenu par l'équipe du <a href="https://nautilus.ljv.fr/">Nautilus</a></li>
 	</ul>
 	<ul>
 		<li>Version 3.1-dev</li>
 		<li><a href="https://github.com/matiboux/LJV-Web-Portal">Projet Open-Source</a></li>
-		<li>Questions, Signalements ? <a href="mailto:matiboux@gmail.com">Contactez moi</a></li>
+		<li>Questions, Signalements ? <a href="mailto:matiboux@gmail.com">Contactez le développeur</a></li>
 	</ul>
 </div>
