03d493f2c8cf308862706bdd4d7d5491431bfdb7
Matiboux <matiboux@gmail.com>
2017-04-01 21:42:06 +0200

    Updated about.php file (legal)

--- about.php
+++ about.php
@@ -29,46 +29,50 @@ $cookieName = 'disableRedirect';
 <?php include 'header.php'; ?>
 <div id="main">
 	<div class="container text-justify">
-		<h2>La raison d'être de <a href="https://ljv.fr/">ljv.fr</a></h2>
+		<h3>La raison d'être de <a href="https://ljv.fr/">ljv.fr</a></h3>
 		<p>Ce site internet a été créé d'après une initiative non-officielle de quelques étudiants du lycée Jules Verne. La plateforme derrière le site a pour objectif de <b>permettre l'hébergement de certains projets plus ou moins liés au lycée</b>, et ainsi de faciliter leur réalisation et leur promotion.</p>
 		<p>L'organisation du domaine a été pensée afin d'offir une certaine visibilité aux différents projets proposés, mais aussi de faciliter l'accès au <a href="jules-verne.paysdelaloire.e-lyco.fr">site internet officiel du lycée Jules Verne</a> (jules-verne.paysdelaloire.e-lyco.fr), via <a href="https://ljv.fr/">le portail web de la plateforme</a> (ljv.fr).</p>
 		
-		<h2>Les projets mis en avant</h2>
+		<h3>Les projets mis en avant</h3>
 		<p>La plateforme ne met en avant qu'un seul projet pour le moment :</p>
 		<ul>
 			<li><a href="https://nautilus.ljv.fr/"><b>Le Nautilus</b></a> (nautilus.ljv.fr), le journal du lycée Jules Verne – un projet réalisé par un groupe de lycéens.</li>
 		</ul>
-		<p>Cependant, celle-ci devrait se doter d'un tout nouveau projet dans les prochains mois :</p>
+		<?php /*<p>Cependant, celle-ci devrait se doter d'un tout nouveau projet dans les prochains mois :</p>
 		<ul>
 			<li><b>FileDrop</b>, un cloud pour le partage rapide de documents, hébergés temporairement en ligne.</li>
+		</ul>*/ ?>
+		<p>Pour le moment, la plateforme est en construction. À terme, la plateforme devrait pouvoir permettre l'hébergement et la création de nouveaux projets, scolaires ou plus personnels.</p> <hr />
+		
+		<h3>Responsable de la plateforme</h3>
+		<p>La plateforme de ce site internet est gérées par <b>Matiboux</b> (Mathieu Guérin).</p>
+		<p>Ce dernier se charge également..</p>
+		<ul>
+			<li>Du nom de domaine <a href="https://ljv.fr/">ljv.fr</a>.</li>
+			<li>De modérer les projets hébergés sur la plateforme.</li>
+			<li>De maintenir ce site internet, ainsi que celui du <a href="https://nautilus.ljv.fr/">Nautilus</a> (nautilus.ljv.fr).</li>
 		</ul>
-		<p>Pour le moment, la plateforme est en construction. À terme, la plateforme devrait pouvoir permettre l'hébergement et la création de nouveaux projets, scolaires ou plus personnels.</p>
-		<?php /*<p>Le responsable de la plateforme est en train de réfléchir au développement d'un cloud ouvert, où les documents hébergés pourrait être visionnés directement depuis un navigateur (pour les images, les pages web, les pdf..)</p>*/ // THIS IS JUST AN IDEA. Although, I already have some scrap code of a cloud project I've made */ ?> <hr />
+		<p>Informations de contact :</p>
+		<ul>
+			<li>Email : <a href="mailto:matiboux@gmail.com">matiboux@gmail.com</a></li>
+			<li>Telegram : <a href="https://t.me/Matiboux">@Matiboux</a></li>
+		</ul>
 		
-		<h2>L'équipe</h2>
-			<h3>Responsable de la plateforme</h3>
-			<p>La plateforme et l'hébergement de ce site internet sont gérés par <b>Mathieu Guérin</b> (alias Matiboux), lycéen en classe de 1<sup>ère</sup> S2 au lycée Jules Verne.</p>
-			<p>Ce dernier est aussi..</p>
-			<ul>
-				<li>Responsable du nom de domaine <a href="https://ljv.fr/">ljv.fr</a>.</li>
-				<li>Propriétaire du serveur d’hébergement web (hébergé chez <a href="https://www.pulseheberg.com/">PulseHeberg</a> et partagé avec la plateforme personnelle de l'élève – indépendante de celle de <a href="https://ljv.fr/">ljv.fr</a>).</li>
-				<li>Modérateur des projets hébergés sur la plateforme</li>
-				<li>Webmaster du site internet <a href="https://ljv.fr/">ljv.fr</a> – ainsi que celui du <a href="https://nautilus.ljv.fr/">Nautilus</a> (nautilus.ljv.fr)</li>
-			</ul>
-			<p>Informations de contact :</p>
-			<ul>
-				<li>Email : <a href="mailto:matiboux@gmail.com">matiboux@gmail.com</a></li>
-				<li>Telegram : <a href="https://t.me/Matiboux">@Matiboux</a></li>
-			</ul>
-			
-			<h3>Le Nautilus</h3>
-			<p>Le Nautilus est géré par Adrien Granjon, rédacteur en chef et responsable de la publication. Ce projet étant étroitement lié à la plateforme <a href="https://ljv.fr/">ljv.fr</a>, ce dernier obtient alors un rôle important dans l'organisation de celle-ci.</p>
-			<p>Informations de contact :</p>
-			<ul>
-				<li>Email : <a href="adrien.granjon@free.fr">adrien.granjon@free.fr</a></li>
-			</ul> <hr />
+		<h3>Responsable du la publication</h3>
+		<p>La publication est modérée par <b>Adrien Granjon</b>, redacteur en chef du <a href="https://nautilus.ljv.fr/">Nautilus</a>.</p>
+		<p>Informations de contact :</p>
+		<ul>
+			<li>Email : <a href="adrien.granjon@free.fr">adrien.granjon@free.fr</a></li>
+		</ul>
 		
-		<h2>Un projet libre</h2>
+		<h3>Hébergement</h3>
+		<p>La plateforme est hébergée chez <a href="https://www.pulseheberg.com/">PulseHeberg</a>.</p>
+		<p>Informations pratiques :</p>
+		<ul>
+			<li>Adresse : 16-18 avenue de L'Europe, 78140 Vélizy-Villacoublay</li>
+		</ul> <hr />
+		
+		<h3>Un projet libre</h3>
 		<p><b>Le projet est désormais un projet ouvert et open-source!</b></p>
 		<p>Le code source du site internet est entièrement disponible sur <a href="https://github.com/matiboux/LJV-Web-Portal">sa page Github</a> sous la <a href="https://fr.wikipedia.org/wiki/Licence_MIT">license MIT</a>. Depuis cet endroit, vous pourrez suivre l'avancée du développement du site et peut être y contribuer, si vous avez des compétences en développement web.
 	</div>
