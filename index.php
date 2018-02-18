a63a03a3da1b2978169663d167b5e77cae6b3347
Matiboux <matiboux@gmail.com>
2018-02-18 17:49:17 +0100

    Added a favicon to the website

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $cookieName = 'disableRedirect';
 <meta name="description" content="Portail web non officiel du Lycée Jules Verne" />
 <meta name="keywords" content="portail,portal,web,internet,lycée,high,school,jules,verne,Nautilus,Matiboux" />
 
+<link rel="shortcut icon" type="image/png" href="https://ljv.fr/favicon.png" />
 <link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/font-awesome.min.css" />
 <link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/dark-navbar.css" />
